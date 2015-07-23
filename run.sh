2d9ad3b7e94791c474e00845fb1c0449852c6613
Vranac Srdjan <vranac@code4hire.com>
2015-07-23 13:25:43 +0200

    Run the run script from workshop root.

--- run.sh
+++ run.sh
@@ -7,12 +7,9 @@ git submodule update
 # ezsylius setup
 ( cd workshops/ezpublish-community-sylius &&
   git checkout sylius_integration &&
-  git pull origin sylius_integration
-)
-
-( cd workshops/ezpublish-community-sylius/installation &&
+  git pull origin sylius_integration &&
   sudo ln -sf /var/www/ezpublish/workshops/ezpublish-community-sylius/installation/vhost /etc/apache2/sites-enabled/ezsylius.conf &&
-  sudo sh ./run.sh
+  sudo sh ./installation/run.sh
 )
 
 sudo service apache2 reload
